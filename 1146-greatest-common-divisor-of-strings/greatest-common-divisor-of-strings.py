2744af496fd458e5fd2735728c8e508ae5122d13
asuracoding <kesbor7@gmail.com>
2025-02-06 14:16:37 +0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (80.12%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -2,5 +2,7 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1+str2 != str2+str1:
             return ""
-        gl = gcd(len(str1), len(str2))
-        return str1[:gl]
+        else:
+            same = gcd(len(str1), len(str2))
+            return str2[:same]
+        
