03b6ce9fa17893a675cb5a4f6bb9c4e6b82d110e
asuracoding <kesbor7@gmail.com>
2025-02-07 12:20:34 +0700

    Time: 326 ms (5.02%) | Memory: 19 MB (7.75%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,9 +1,16 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = {'a', 'i', 'u','e','o'}
-        total = max_windows = sum([1 if x in vowels else 0 for x in s[:k]])
+        vowels = {'a','i','u','e','o'}
+
+        window = s[:k]
+        current_vowels = max_vowels = len([x for x in window if x in vowels])
         for x in range(len(s) - k):
-            max_windows = max_windows -(1 if s[x] in vowels else 0) + (1 if s[x + k] in vowels else 0)
+            print(f"removing {s[x]} and got {s[x+k]}")
+            if s[x] in vowels:
+                current_vowels -=1
             
-            total = max(total, max_windows)
-        return total
+            if s[x + k] in vowels:
+                current_vowels +=1
+                max_vowels = max(current_vowels, max_vowels)
+            print(f"total are {max_vowels}")
+        return max_vowels
