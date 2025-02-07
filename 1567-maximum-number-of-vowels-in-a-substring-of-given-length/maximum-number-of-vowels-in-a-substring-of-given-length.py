c9025176ba3860b8bfa25b0ce497e0c5db233abf
asuracoding <kesbor7@gmail.com>
2025-02-07 12:21:03 +0700

    Time: 48 ms (95.97%) | Memory: 18.9 MB (7.75%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -5,12 +5,10 @@ class Solution:
         window = s[:k]
         current_vowels = max_vowels = len([x for x in window if x in vowels])
         for x in range(len(s) - k):
-            print(f"removing {s[x]} and got {s[x+k]}")
             if s[x] in vowels:
                 current_vowels -=1
             
             if s[x + k] in vowels:
                 current_vowels +=1
                 max_vowels = max(current_vowels, max_vowels)
-            print(f"total are {max_vowels}")
         return max_vowels
